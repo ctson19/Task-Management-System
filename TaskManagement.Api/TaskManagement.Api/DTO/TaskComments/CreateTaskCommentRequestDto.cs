8a994ce26d4da10f3bff6895da49bd7d52a22310
dinamok59 <dinamok59@gmail.com>
2026-02-15 22:44:50 +0700

    push

---
+++ TaskManagement.Api/TaskManagement.Api/DTO/TaskComments/CreateTaskCommentRequestDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskManagement.Api.DTO.TaskComments
+{
+    public class CreateTaskCommentRequestDto
+    {
+        public string Content { get; set; } = null!;
+    }
+}
+
+
